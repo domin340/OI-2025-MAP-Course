a9b5abd02952b2b7c1e40e9aa3d9c22557e81c27
domin340 <poweski2009@gmail.com>
2025-04-22 19:42:17 +0200

    poc

---
+++ poc/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Mount Everest - 8850 m n.p.m.\n\
+McKinley - 6194 m n.p.m.\n\
+Mont Blanc - 4810 m n.p.m."
+              << std::endl;
+
+    return 0;
+}

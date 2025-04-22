863ee1e51e0a7b022ae4c0cad944a8a006c1b41b
domin340 <poweski2009@gmail.com>
2025-04-22 20:28:05 +0200

    nao minor changes to array

--- nao/main.cpp
+++ nao/main.cpp
@@ -6,16 +6,13 @@ int main() {
     int nums[3];
 
     for (int i = 0; i < MAX_N; i++) {
-        int num;
-        std::cin >> num;
+        std::cin >> nums[i];
 
-        if (num < -1000 || num > 1000) {
+        if (nums[i] < -1000 || nums[i] > 1000) {
             std::cout << "provided n has to be in range of (-1000<=n<=1000)"
                       << std::endl;
             return 1;
         }
-
-        nums[i] = num;
     }
 
     for (int i = MAX_N - 1; i >= 0; i--) {
